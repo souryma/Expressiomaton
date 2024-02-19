5da9126eff8cf8e4e095dae49579de5fa1c00c9d
antoine.martin <antoine.martin885@gmail.com>
2024-02-19 19:36:04 +0100

    Added emotion fer plus

---
+++ Assets/EmotionFerPlus/Test.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using Unity.Barracuda;
+using System.Linq;
+
+namespace EmotionFerPlus {
+
+sealed class Test : MonoBehaviour
+{
+    #region Editable attributes
+
+    [SerializeField] Texture2D _image = null;
+    [SerializeField] NNModel _model = null;
+    [SerializeField] ComputeShader _preprocessor = null;
+    [SerializeField] UnityEngine.UI.RawImage _preview = null;
+    [SerializeField] UnityEngine.UI.Text _label = null;
+
+    #endregion
+
+    #region Compile-time constants
+
+    const int ImageSize = 64;
+
+    readonly static string[] Labels =
+      { "Neutral", "Happiness", "Surprise", "Sadness",
+        "Anger", "Disgust", "Fear", "Contempt"};
+
+    #endregion
+
+    #region MonoBehaviour implementation
+
+    void Start()
+    {
+        using var worker = ModelLoader.Load(_model).CreateWorker();
+
+        // Preprocessing
+        using var preprocessed = new ComputeBuffer(ImageSize * ImageSize, sizeof(float));
+
+        WebCamTexture webCamTexture = new WebCamTexture();
+        webCamTexture.Play();
+
+        _preprocessor.SetTexture(0, "_Texture", webCamTexture);
+        _preprocessor.SetBuffer(0, "_Tensor", preprocessed);
+        _preprocessor.Dispatch(0, ImageSize / 8, ImageSize / 8, 1);
+
+        // Emotion recognition model
+        using (var tensor = new Tensor(1, ImageSize, ImageSize, 1, preprocessed))
+            worker.Execute(tensor);
+
+        // Output aggregation
+        var probs = worker.PeekOutput().AsFloats().Select(x => Mathf.Exp(x));
+        var sum = probs.Sum();
+        var lines = Labels.Zip(probs, (l, p) => $"{l,-12}: {p / sum:0.00}");
+        _label.text = string.Join("\n", lines);
+
+        _preview.texture = webCamTexture;
+    }
+
+    #endregion
+}
+
+} // namespace EmotionFerPlus
