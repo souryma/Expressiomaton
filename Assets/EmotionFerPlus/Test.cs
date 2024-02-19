527ee1e0fbbcf1324a697bac8f5d949816760c87
souryma <antoine.martin885@gmail.com>
2024-02-19 20:02:06 +0100

    Working ferplus scene with camera

--- Assets/EmotionFerPlus/Test.cs
+++ Assets/EmotionFerPlus/Test.cs
@@ -8,15 +8,19 @@ sealed class Test : MonoBehaviour
 {
     #region Editable attributes
 
-    [SerializeField] Texture2D _image = null;
     [SerializeField] NNModel _model = null;
-    [SerializeField] ComputeShader _preprocessor = null;
     [SerializeField] UnityEngine.UI.RawImage _preview = null;
     [SerializeField] UnityEngine.UI.Text _label = null;
+    [SerializeField] ComputeShader _preprocessor = null;
 
     #endregion
 
-    #region Compile-time constants
+    #region Internal objects
+
+    WebCamTexture _webcamRaw;
+    RenderTexture _webcamBuffer;
+    ComputeBuffer _preprocessed;
+    IWorker _worker;
 
     const int ImageSize = 64;
 
@@ -30,29 +34,56 @@ sealed class Test : MonoBehaviour
 
     void Start()
     {
-        using var worker = ModelLoader.Load(_model).CreateWorker();
+        _webcamRaw = new WebCamTexture("Integrated Camera");
+        _webcamBuffer = new RenderTexture(512, 512, 0);
+        _preprocessed = new ComputeBuffer(ImageSize * ImageSize, sizeof(float));
+        _worker = ModelLoader.Load(_model).CreateWorker();
+
+        _webcamRaw.Play();
+        _preview.texture = _webcamBuffer;
+        
+        foreach (var device in WebCamTexture.devices)
+        {
+            Debug.Log(device.name);
+        }
+    }
+
+    void OnDisable()
+    {
+        _preprocessed?.Dispose();
+        _preprocessed = null;
+
+        _worker?.Dispose();
+        _worker = null;
+    }
+
+    void OnDestroy()
+    {
+        if (_webcamRaw != null) Destroy(_webcamRaw);
+        if (_webcamBuffer != null) Destroy(_webcamBuffer);
+    }
+
+    void Update()
+    {
+        // Cropping
+        var scale = new Vector2((float)_webcamRaw.height / _webcamRaw.width, 1);
+        var offset = new Vector2(scale.x / 2, 0);
+        Graphics.Blit(_webcamRaw, _webcamBuffer, scale, offset);
 
         // Preprocessing
-        using var preprocessed = new ComputeBuffer(ImageSize * ImageSize, sizeof(float));
-
-        WebCamTexture webCamTexture = new WebCamTexture();
-        webCamTexture.Play();
-
-        _preprocessor.SetTexture(0, "_Texture", webCamTexture);
-        _preprocessor.SetBuffer(0, "_Tensor", preprocessed);
+        _preprocessor.SetTexture(0, "_Texture", _webcamBuffer);
+        _preprocessor.SetBuffer(0, "_Tensor", _preprocessed);
         _preprocessor.Dispatch(0, ImageSize / 8, ImageSize / 8, 1);
 
         // Emotion recognition model
-        using (var tensor = new Tensor(1, ImageSize, ImageSize, 1, preprocessed))
-            worker.Execute(tensor);
+        using (var tensor = new Tensor(1, ImageSize, ImageSize, 1, _preprocessed))
+            _worker.Execute(tensor);
 
         // Output aggregation
-        var probs = worker.PeekOutput().AsFloats().Select(x => Mathf.Exp(x));
+        var probs = _worker.PeekOutput().AsFloats().Select(x => Mathf.Exp(x));
         var sum = probs.Sum();
         var lines = Labels.Zip(probs, (l, p) => $"{l,-12}: {p / sum:0.00}");
         _label.text = string.Join("\n", lines);
-
-        _preview.texture = webCamTexture;
     }
 
     #endregion
