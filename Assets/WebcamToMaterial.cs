1fbe0d46c0f943feb3e6c2abc102e20bbae73102
souryma <antoine.martin885@gmail.com>
2024-03-09 12:18:06 +0100

    Added camera to cowboy face

--- Assets/WebcamToMaterial.cs
+++ Assets/WebcamToMaterial.cs
@@ -6,12 +6,27 @@ public class WebcamToMaterial : MonoBehaviour
 {
     [SerializeField] private MeshRenderer targetMaterial;
 
+    public enum Cameras
+    {
+        Webcam1,
+        Webcam2
+    }
+
+    [SerializeField] private Cameras _cameras = Cameras.Webcam1;
+
     // Start is called before the first frame update
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            targetMaterial.material.mainTexture = WebcamManager.instance.Webcam1Texture;
+            if (_cameras == Cameras.Webcam1)
+            {
+                targetMaterial.material.mainTexture = WebcamManager.instance.Webcam1Texture;
+            }
+            else
+            {
+                targetMaterial.material.mainTexture = WebcamManager.instance.Webcam2Texture;
+            }
         }
     }
 }
